7da19017c3f76c4f1568766beb434455e70069a9
RadBar2 <rad.bartosevic@gmail.com>
2024-02-01 17:43:48 +0200

    2
    2nd commit

--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Cursor : MonoBehaviour
 {
+    public TextMeshProUGUI counter;
+
+    int score = 0;
     private void OnCollisionEnter(Collision collision)
     {
         Destroy(collision.gameObject);
+        counter.text = (++score).ToString();
     }
 }
